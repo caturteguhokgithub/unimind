22583e405a5c210ea18805f5bf005c4b8661ab14
caturteguhokgithub <caturteguhok@gmail.com>
2024-10-02 05:59:17 +0700

    dashboard view

---
+++ src/app/dashboard/view/page.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import PageDashboard from '@/app/dashboard/page';
+
+export default function PageOverview(): React.JSX.Element {
+  return <PageDashboard title="News Media / Overview" />;
+}
--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,12 +1,34 @@
 'use client';
 
 import * as React from 'react';
-import { Autocomplete, Container, FormControl, TextField, Typography } from '@mui/material';
+import Image from 'next/image';
+import Link from 'next/link';
+import {
+  Autocomplete,
+  Avatar,
+  Button,
+  Container,
+  Divider,
+  FormControl,
+  ListItemIcon,
+  ListItemText,
+  MenuItem,
+  MenuList,
+  Paper,
+  Popover,
+  TextField,
+  Typography,
+} from '@mui/material';
 import Box from '@mui/material/Box';
+import { red } from '@mui/material/colors';
 import Stack from '@mui/material/Stack';
+import { Cloud } from '@phosphor-icons/react';
 
+import { paths } from '@/paths';
 import { usePopover } from '@/hooks/use-popover';
 import DateRangePicker from '@/components/dateRange';
+
+import { IconFA } from '../icons/icon-fa';
 
 const listKodeKl = [
   {
@@ -35,7 +57,7 @@ const listKodeKl = [
   },
 ];
 
-export function HeaderNav({ title }: { title: string }): React.JSX.Element {
+export function HeaderNav({ title, isDashboardView }: { title: string; isDashboardView?: boolean }): React.JSX.Element {
   const [valueKl, setValueKl] = React.useState<string | null>('');
   const [inputValueKl, setInputValueKl] = React.useState('');
   const [klCode, setKlCode] = React.useState('');
@@ -50,6 +72,19 @@ export function HeaderNav({ title }: { title: string }): React.JSX.Element {
     return item['name'];
   });
 
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
   return (
     <Box
       component="header"
@@ -63,9 +98,32 @@ export function HeaderNav({ title }: { title: string }): React.JSX.Element {
     >
       <Container maxWidth="xl">
         <Stack direction="row" spacing={2} justifyContent="space-between" alignItems="center" height={64}>
-          <Typography component="h2" fontSize="1.5rem" fontWeight={500}>
-            {title}
-          </Typography>
+          {isDashboardView ? (
+            <Stack direction="row" justifyContent="center" sx={{ userSelect: 'none' }}>
+              <Stack justifyContent="center" height={64}>
+                <Box component={Link} href={paths.home} sx={{ display: 'inline-flex' }}>
+                  <Image
+                    src="https://res.cloudinary.com/caturteguh/image/upload/v1727302971/unimind/logo-unimind_f1ehza.png"
+                    alt="unimind"
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    style={{
+                      width: '76px',
+                      height: 'auto',
+                    }}
+                  />
+                </Box>
+                <Typography color="black" fontSize={16} fontWeight={600}>
+                  Media Monitoring Dashboard
+                </Typography>
+              </Stack>
+            </Stack>
+          ) : (
+            <Typography component="h2" fontSize="1.5rem" fontWeight={500}>
+              {title}
+            </Typography>
+          )}
           <Stack alignItems="center" direction="row" spacing={2}>
             <FormControl fullWidth>
               <Autocomplete
@@ -105,6 +163,45 @@ export function HeaderNav({ title }: { title: string }): React.JSX.Element {
               />
             </FormControl>
             <DateRangePicker placeholder="Pilih tanggal" />
+            <Button aria-describedby={id} size="small" onClick={handleClick} sx={{ p: 0, minWidth: 'auto' }}>
+              <Avatar
+                alt="Account"
+                src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=300&h=300&q=80"
+                sx={{ width: 40, height: 40 }}
+              />
+            </Button>
+            <Popover
+              id={id}
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+            >
+              <Paper sx={{ width: 200, maxWidth: '100%' }}>
+                <MenuList>
+                  <MenuItem>
+                    <ListItemIcon sx={{ minWidth: '32px !important' }}>
+                      <IconFA name="user-gear" size={14} sx={{ width: 20 }} />
+                    </ListItemIcon>
+                    <ListItemText>Account</ListItemText>
+                  </MenuItem>
+                  <Divider />
+                  <MenuItem>
+                    <ListItemIcon>
+                      <IconFA name="right-from-bracket" size={14} sx={{ width: 20, color: red[600] }} />
+                    </ListItemIcon>
+                    <ListItemText sx={{ color: red[600] }}>Logout</ListItemText>
+                  </MenuItem>
+                </MenuList>
+              </Paper>
+            </Popover>
           </Stack>
         </Stack>
       </Container>
